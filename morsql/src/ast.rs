8ed1006e21732359863e2f5af18f037171bc3bc2
Harrison Goldstein <hgoldstein95@gmail.com>
2021-03-23 21:01:24 -0400

    parser: Made good progress, need to add PBT

--- morsql/src/ast.rs
+++ morsql/src/ast.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::fmt;
+
 pub type Ident = String;
 pub type Table = String;
 
@@ -10,6 +12,16 @@ pub struct Query {
   filter: Filter,
 }
 
+impl Query {
+  pub fn new(s: Selection, t: Vec<Table>, f: Filter) -> Self {
+    Query {
+      selection: s,
+      tables: t,
+      filter: f,
+    }
+  }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Selection {
   Star,
@@ -18,7 +30,19 @@ pub enum Selection {
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum UnaryOp {
-  Neg,
+  Not,
+}
+
+impl fmt::Display for UnaryOp {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(
+      f,
+      "{}",
+      match self {
+        UnaryOp::Not => "NOT",
+      }
+    )
+  }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -30,6 +54,22 @@ pub enum BinaryOp {
   Like,
 }
 
+impl fmt::Display for BinaryOp {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(
+      f,
+      "{}",
+      match self {
+        BinaryOp::And => "&&",
+        BinaryOp::Or => "||",
+        BinaryOp::Eq => "==",
+        BinaryOp::Lt => "<",
+        BinaryOp::Like => "LIKE",
+      }
+    )
+  }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Filter {
   Id(Ident),
@@ -38,4 +78,17 @@ pub enum Filter {
   LitI(i64),
   UnaryOp(UnaryOp, Box<Filter>),
   BinaryOp(BinaryOp, Box<Filter>, Box<Filter>),
+}
+
+impl fmt::Display for Filter {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Filter::Id(i) => write!(f, "{}", i),
+      Filter::LitS(s) => write!(f, "\"{}\"", s),
+      Filter::LitB(b) => write!(f, "{}", b),
+      Filter::LitI(n) => write!(f, "{}", n),
+      Filter::UnaryOp(op, e) => write!(f, "({} {})", op, e),
+      Filter::BinaryOp(op, e1, e2) => write!(f, "({} {} {})", e1, op, e2),
+    }
+  }
 }
--- morsql/src/parser.rs
+++ morsql/src/parser.rs
@@ -7,7 +7,9 @@ use nom::character::complete::*;
 use nom::combinator::map;
 use nom::error::{context, VerboseError};
 use nom::multi::fold_many0;
+use nom::multi::separated_list1;
 use nom::sequence::pair;
+use nom::sequence::terminated;
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
 
@@ -42,53 +44,93 @@ fn pstring(input: &str) -> Res<&str, String> {
 fn factor(input: &str) -> Res<&str, Filter> {
   fn not(i: &str) -> Res<&str, Filter> {
     let (i, _) = tag_no_case("NOT")(i)?;
+    let (i, _) = space0(i)?;
     let (i, f) = factor(i)?;
-    Ok((i, Filter::UnaryOp(UnaryOp::Neg, Box::new(f))))
+    Ok((i, Filter::UnaryOp(UnaryOp::Not, Box::new(f))))
   }
 
   context(
     "factor",
     alt((
-      map(ident, Filter::Id),
+      delimited(char('('), expression, char(')')),
+      not,
       map(digit1, |x: &str| Filter::LitI(x.parse::<i64>().unwrap())),
       map(pbool, Filter::LitB),
       map(pstring, Filter::LitS),
-      not,
-      delimited(char('('), expression, char(')')),
+      map(ident, Filter::Id),
     )),
   )(input)
 }
 
 fn term(input: &str) -> Res<&str, Filter> {
   let (input, init) = factor(input)?;
-  fold_many0(
-    pair(tag("&&"), factor),
-    init,
-    |acc: Filter, (op, f): (&str, Filter)| match op {
-      "&&" => Filter::BinaryOp(BinaryOp::And, Box::new(acc), Box::new(f)),
-      _ => panic!("invalid term op"),
-    },
+  let (input, _) = space0(input)?;
+
+  context(
+    "term",
+    fold_many0(
+      pair(
+        alt((
+          terminated(alt((tag("&&"), tag("=="), tag("<"))), space0),
+          terminated(tag_no_case("LIKE"), space0),
+        )),
+        factor,
+      ),
+      init,
+      |acc: Filter, (op, f): (&str, Filter)| match op.to_lowercase().as_str() {
+        "&&" => Filter::BinaryOp(BinaryOp::And, Box::new(acc), Box::new(f)),
+        "==" => Filter::BinaryOp(BinaryOp::Eq, Box::new(acc), Box::new(f)),
+        "<" => Filter::BinaryOp(BinaryOp::Lt, Box::new(acc), Box::new(f)),
+        "like" => Filter::BinaryOp(BinaryOp::Like, Box::new(acc), Box::new(f)),
+        _ => panic!("invalid term op"),
+      },
+    ),
   )(input)
 }
 
 fn expression(input: &str) -> Res<&str, Filter> {
   let (input, init) = term(input)?;
-  fold_many0(
-    pair(tag("||"), term),
-    init,
-    |acc: Filter, (op, f): (&str, Filter)| match op {
-      "||" => Filter::BinaryOp(BinaryOp::Or, Box::new(acc), Box::new(f)),
-      _ => panic!("invalid factor op"),
-    },
+  let (input, _) = space0(input)?;
+
+  context(
+    "expression",
+    fold_many0(
+      pair(terminated(tag("||"), space0), term),
+      init,
+      |acc: Filter, (op, f): (&str, Filter)| match op {
+        "||" => Filter::BinaryOp(BinaryOp::Or, Box::new(acc), Box::new(f)),
+        _ => panic!("invalid factor op"),
+      },
+    ),
   )(input)
 }
 
-fn selection(_input: &str) -> Res<&str, Selection> {
-  unimplemented!()
+fn selection(input: &str) -> Res<&str, Selection> {
+  context(
+    "selection",
+    alt((
+      map(tag("*"), |_| Selection::Star),
+      map(separated_list1(terminated(tag(","), space0), ident), |xs| {
+        Selection::Columns(xs)
+      }),
+    )),
+  )(input)
 }
 
-fn query(_input: &str) -> Res<&str, Query> {
-  unimplemented!()
+fn query(input: &str) -> Res<&str, Query> {
+  let (input, _) = tag_no_case("SELECT")(input)?;
+  let (input, _) = multispace1(input)?;
+  let (input, s) = selection(input)?;
+  let (input, _) = multispace1(input)?;
+  let (input, _) = tag_no_case("FROM")(input)?;
+  let (input, _) = multispace1(input)?;
+  let (input, t) = separated_list1(terminated(tag(","), space0), ident)(input)?;
+  let (input, _) = multispace1(input)?;
+  let (input, _) = tag_no_case("WHERE")(input)?;
+  let (input, _) = space1(input)?;
+  let (input, f) = expression(input)?;
+
+  Ok((input, Query::new(s, t, f)))
 }
 
 #[cfg(test)]
@@ -96,7 +138,44 @@ mod tests {
   use super::*;
 
   #[test]
-  fn parse_filter() {
+  fn whole_query() {
+    assert_eq!(
+      query("SELECT name, id\nFROM users\nWHERE name == \"Harry\""),
+      Ok((
+        "",
+        Query::new(
+          Selection::Columns(vec!["name".to_owned(), "id".to_owned()]),
+          vec!["users".to_owned()],
+          Filter::BinaryOp(
+            BinaryOp::Eq,
+            Box::new(Filter::Id("name".to_owned())),
+            Box::new(Filter::LitS("Harry".to_owned()))
+          )
+        )
+      ))
+    )
+  }
+
+  #[test]
+  fn filter_not() {
+    assert_eq!(
+      expression("NOT x && y"),
+      Ok((
+        "",
+        Filter::BinaryOp(
+          BinaryOp::And,
+          Box::new(Filter::UnaryOp(
+            UnaryOp::Not,
+            Box::new(Filter::Id("x".to_owned()))
+          )),
+          Box::new(Filter::Id("y".to_owned()))
+        )
+      ))
+    );
+  }
+
+  #[test]
+  fn filter_and_or() {
     let x = Filter::Id("x".to_owned());
     let y = Filter::Id("y".to_owned());
     let z = Filter::Id("z".to_owned());
@@ -142,7 +221,7 @@ mod tests {
     );
 
     assert_eq!(
-      expression("(x&&y)&&z"),
+      expression("(x && y) && z"),
       Ok((
         "",
         Filter::BinaryOp(
@@ -158,7 +237,7 @@ mod tests {
     );
 
     assert_eq!(
-      expression("x&&(y&&z)"),
+      expression("x&&  (y&&z)"),
       Ok((
         "",
         Filter::BinaryOp(
