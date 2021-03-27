f5de6366601a011aa55d0696d632f9436f9a82e7
Harrison Goldstein <hgoldstein95@gmail.com>
2021-03-26 22:35:06 -0400

    main: Set up the simplest possible main function.

--- morsql/src/main.rs
+++ morsql/src/main.rs
@@ -5,11 +5,22 @@ extern crate quickcheck;
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
 
+use std::env;
+use std::error;
+use std::fs;
+
 mod ast;
 mod parser;
 
-fn main() -> Result<(), String> {
-  let query = parser::parse_sql("SELECT id, name FROM users where name == \"Harry\"")?;
+fn main() -> Result<(), Box<dyn error::Error + 'static>> {
+  let args: Vec<String> = env::args().collect();
+  let query_file = &args[1];
+  let query_text = fs::read_to_string(query_file)?;
+
+  let query = parser::parse_sql(&query_text)?;
+
+  // TODO: Run query
   println!("{}", query);
+
   Ok(())
 }
