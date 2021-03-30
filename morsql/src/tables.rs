88f30979394dd3218acf7743077e88fb73af9651
Lucas Silver <lis32@cornell.edu>
2021-03-30 11:44:41 -0400

    removed panics that might come up in normally behaving code, and added tests for the join function

--- morsql/src/tables.rs
+++ morsql/src/tables.rs
@@ -6,6 +6,7 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use regex::Regex;
 use std::fs::File;
+use std::io::Error;
 
 
 
@@ -15,11 +16,13 @@ pub enum TableCell{
     CellString(String)
 }
 
-#[derive(Debug,PartialEq, Eq)]
+#[derive(Debug,PartialEq, Eq, Clone)]
 pub struct TableData{
     pub header : Vec<String>,
     pub rows : Vec<Vec<Option<TableCell>>>
 }
+
+
 
 impl ColumnSelector {
     pub fn to_indexed(&self, tables : &Vec<TableData>, names : &Vec<String>) -> Result<IndexedColumnSelector, String>{
@@ -28,20 +31,21 @@ impl ColumnSelector {
         match &self.table {
             Some(name) => {
                 table_index = names.iter().position(|n| *n == *name)
-                    .expect(format!("Specified table {} was not found for {}", name, self));
-                column_index= tables[table_index].header.iter().position(|&id| id == self.field)
-                    .expect(format!("Specified column {} was not found for in table {} for {}", self.field, name, self));
+                    .ok_or(format!("Specified table {} was not found for {}", name, self))?;
+                
+                column_index = tables[table_index].header.iter().position(|id| *id == self.field)
+                    .ok_or(format!("Specified column {} was not found for in table {} for {}", self.field, name, self))?;
 
             },
             None => {
                 let mut column_index_opt : Option<usize> = None;
                 table_index = tables.iter().position( |table| {
-                    column_index_opt = table.header.iter().position(|&id| id == self.field);
+                    column_index_opt = table.header.iter().position(|id| *id == self.field);
                     return matches!(column_index_opt, Some(x));
-                }).expect(format!("Specified column {} was not found for in tables {:?} for {}", self.field, names, self));
-                column_index = column_index_opt.unwrap();
+                }).ok_or(format!("Specified column {} was not found for in tables {:?} for {}", self.field, names, self))?;
+                column_index = column_index_opt.ok_or("".to_string())?;
             }
-        }
+        } 
         return Ok(IndexedColumnSelector{
             table : table_index,
             field : column_index
@@ -50,7 +54,7 @@ impl ColumnSelector {
 }
 
 impl TableData {
-    pub fn of_file(file : File) -> Result<Self, String>{
+    pub fn of_file(file : File) -> Result<Self, Error>{
         let mut header : Option<Vec<String>> = None;
         let mut rows : Vec<Vec<Option<TableCell>>> = Vec::new();
         let re = Regex::new("\".*\"").unwrap();
@@ -135,6 +139,53 @@ mod tests {
         curr_dir.push(Path::new("examples/test1.csv"));
         let filetable = TableData::of_file(File::open(curr_dir).unwrap()).unwrap();
         assert_eq!(testtable, filetable);
+    }
+
+    #[test]
+    fn join_test(){
+        let test_header1 = vec!["name".to_string(), "age".to_string(), "id".to_string()];
+        let row11 = vec![Some(TableCell::CellString("Lucas".to_string())), Some(TableCell::CellInt(24)), Some(TableCell::CellInt(0))];
+        let row12 = vec![Some(TableCell::CellString("Harry".to_string())), Some(TableCell::CellInt(25)), Some(TableCell::CellInt(1))];
+        let row13 = vec![Some(TableCell::CellString("".to_string())), None, Some(TableCell::CellInt(2))];
+        let testtable1 = TableData {
+            header: test_header1.clone(),
+            rows : vec![row11.clone(), row12.clone(), row13.clone()]
+        };
+        let test_header2 = vec!["name_".to_string(), "age_".to_string(), "id_".to_string()];
+        let row21 = vec![Some(TableCell::CellString("Lucas_".to_string())), Some(TableCell::CellInt(24)), Some(TableCell::CellInt(0))];
+        let row22 = vec![Some(TableCell::CellString("Harry_".to_string())), Some(TableCell::CellInt(25)), Some(TableCell::CellInt(1))];
+        let row23 = vec![Some(TableCell::CellString("_".to_string())), None, Some(TableCell::CellInt(2))];
+        let testtable2 = TableData {
+            header: test_header2.clone(),
+            rows : vec![row21.clone(), row22.clone(), row23.clone()]
+        };
+
+        let res_table1 = TableData::join_table( |vec| vec[0][1] == vec[1][1] , &(vec![testtable1.clone(), testtable2.clone()]));
+        assert_eq!(res_table1.header, vec![test_header1.clone(), test_header2.clone()].concat());
+        assert_eq!(res_table1.rows.len(), 3);
+        //check rows
+        assert_ne!(res_table1.rows.iter().find(| row | **row == vec![row11.clone(), row21.clone()].concat()), None );
+        assert_ne!(res_table1.rows.iter().find(| row | **row == vec![row12.clone(), row22.clone()].concat()), None );
+        assert_ne!(res_table1.rows.iter().find(| row | **row == vec![row13.clone(), row23.clone()].concat()), None );
+        assert_eq!(res_table1.rows.iter().find(| row | **row == vec![row11.clone(), row22.clone()].concat()), None );
+        let res_table2 = TableData::join_table( |vec| true , &(vec![testtable1.clone(), testtable2.clone()])); 
+        assert_eq!(res_table2.header, vec![test_header1.clone(), test_header2.clone()].concat());
+        assert_eq!(res_table2.rows.len(),9);
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row11.clone(), row21.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row12.clone(), row21.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row13.clone(), row21.clone()].concat()), None );
+
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row11.clone(), row22.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row12.clone(), row22.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row13.clone(), row22.clone()].concat()), None );
+
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row11.clone(), row23.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row12.clone(), row23.clone()].concat()), None );
+        assert_ne!(res_table2.rows.iter().find(| row | **row == vec![row13.clone(), row23.clone()].concat()), None );
+        //the tables appear to be right
+        let res_table3 = TableData::join_table( |vec| false , &(vec![testtable1.clone(), testtable2.clone()])); 
+        assert_eq!(res_table3.header, vec![test_header1.clone(), test_header2.clone()].concat());
+        assert_eq!(res_table3.rows.len(),0);
     }
 
 }
